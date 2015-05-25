58a1f7e21c4f43d1122c66d515b1ec69b26ed5f3
Douglas Melo <dmelo85@gmail.com>
2015-05-25 11:02:31 -0300

    Added find_next_destination method to mesh.rb

--- app/controllers/meshes_controller.rb
+++ app/controllers/meshes_controller.rb
@@ -21,7 +21,8 @@ class MeshesController < ApplicationController
 
       Mesh.by_origin(params[:origin]).each do |mash|
         origins = [params[:origin]]
-        find_next_destination(mash, origins, params[:destination], mash.distance, results)
+        mesh = Mesh.new();
+        mesh.find_next_destination(mash, origins, params[:destination], mash.distance, results)
       end
 
       minimum = results.keys.min
@@ -89,16 +90,4 @@ class MeshesController < ApplicationController
       params[:fuel_value].present?
   end
 
-  def find_next_destination(reference_mash, paths, destination, distance, results)
-    paths << reference_mash.destination
-
-    if reference_mash.destination != destination
-      Mesh.by_origin(reference_mash.destination).each do |mash|
-        distance += mash.distance
-        find_next_destination(mash, paths, destination, distance, results)
-      end
-    else
-      results[distance] = paths.clone
-    end
-  end
 end
--- app/models/mesh.rb
+++ app/models/mesh.rb
@@ -1,4 +1,18 @@
 class Mesh < ActiveRecord::Base
   scope :by_origin, ->(origin = nil) { where(origin: origin) if origin.present? }
   scope :by_destination, ->(destination = nil) { where(destination: destination) if destination.present? }
+
+  def find_next_destination(reference_mash, paths, destination, distance, results)
+    paths << reference_mash.destination
+
+    if reference_mash.destination != destination
+      Mesh.by_origin(reference_mash.destination).each do |mash|
+        distance += mash.distance
+        find_next_destination(mash, paths, destination, distance, results)
+      end
+    else
+      results[distance] = paths.clone
+    end
+  end
+  
 end
